a827378d4efb5a0c1972c443b824ad6d6bfe572b
alejandro <JAGUIR4@bancodebogota.com.co>
2021-04-10 20:35:59 -0500

    mod

--- migrations/20210311155604-inicio.js
+++ migrations/20210311155604-inicio.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const fs = require('fs');
+var path = require('path');
+
 var dbm;
 var type;
 var seed;
@@ -15,11 +18,47 @@ exports.setup = function(options, seedLink) {
 };
 
 exports.up = function(db) {
-  return null;
+  try {
+	let baseFolder = path.join(__dirname, 'tablas');
+        let files = fs.readdirSync(baseFolder);
+        for (let file of files) {
+            console.log(file);
+            let sql = fs.readFileSync(`${baseFolder}/${file}`, 'utf8');
+            db.runSql(sql);
+            console.log('Ok........');
+        }	
+        return db.runSql('COMMIT');    
+  } catch (err) {
+    console.error('Fail......');
+    throw err;
+  }
 };
 
 exports.down = function(db) {
-  return null;
+  console.log('Iniciando Delete........');
+  try {
+	  db.runSql(`
+	    DO $$ DECLARE
+	    BEGIN
+		DROP TABLE IF EXISTS ejercicio;
+		DROP TABLE IF EXISTS objetivo;
+		DROP TABLE IF EXISTS observacion;
+		DROP TABLE IF EXISTS parametro;
+		DROP TABLE IF EXISTS perfil;
+		DROP TABLE IF EXISTS peso;
+		DROP TABLE IF EXISTS plan_entrenamiento;
+		DROP TABLE IF EXISTS rutina_entrenamiento;
+		DROP TABLE IF EXISTS sesion;
+		DROP TABLE IF EXISTS suscripcion;
+		DROP TABLE IF EXISTS tipo_musculo;
+		DROP TABLE IF EXISTS usuario;
+	    END $$;
+	  `);
+	  return db.runSql('COMMIT');
+   } catch (err) {
+    console.error('Fail......');
+    throw err;
+  }
 };
 
 exports._meta = {
